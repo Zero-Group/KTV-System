bf05bef4126922ffd855d65498290050494068be
lihan <fndnh@foxmail.com>
2018-06-11 18:58:33 +0800

    Update the sql file.

--- src/main/resources/sql/database.sql
+++ src/main/resources/sql/database.sql
@@ -1,78 +1,83 @@
-create table tb_food
-(
-  id    int auto_increment,
-  fname varchar(20)    not null,
-  price decimal(10, 2) not null,
-  stock int            not null,
-  primary key (id, fname)
-)
-  engine = InnoDB;
+/*
+ Navicat MySQL Data Transfer
+ Source Schema         : db_ktv
 
-create table tb_manager
-(
-  username varchar(255) not null
-    primary key,
-  password varchar(255) not null
-)
-  engine = InnoDB;
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : 65001
+*/
 
-create table tb_room
-(
-  id         int             not null,
-  type       varchar(10)     not null,
-  price      double          null,
-  status     int default '0' not null,
-  start_time datetime        null,
-  end_time   datetime        null,
-  primary key (type, id)
-)
-  engine = InnoDB;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-create index `index`
-  on tb_room (type);
+-- ----------------------------
+-- Table structure for tb_food
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_food`;
+CREATE TABLE `tb_food`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` decimal(10, 2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  PRIMARY KEY (`id`, `fname`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-create table tb_vip
-(
-  id    int auto_increment,
-  cname varchar(20) not null,
-  phone varchar(20) not null,
-  primary key (id, cname)
-)
-  engine = InnoDB
-  charset = utf8;
+-- ----------------------------
+-- Table structure for tb_manager
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_manager`;
+CREATE TABLE `tb_manager`  (
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-create table tb_order
-(
-  id        int auto_increment
-    primary key,
-  ordertime datetime        null,
-  room      varchar(32)     not null,
-  cname     varchar(30)     not null,
-  phone     varchar(30)     not null,
-  price     int             null,
-  pay       int default '0' null,
-  detail    varchar(500)    null,
-  constraint fk_1
-  foreign key (cname) references tb_vip (cname)
-    on update cascade
-    on delete cascade,
-  constraint fk_2
-  foreign key (phone) references tb_vip (phone)
-    on update cascade
-    on delete cascade
-)
-  engine = InnoDB
-  charset = utf8;
+-- ----------------------------
+-- Table structure for tb_order
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_order`;
+CREATE TABLE `tb_order`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ordertime` datetime(0) DEFAULT NULL,
+  `room` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cname` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  `pay` int(11) DEFAULT 0,
+  `detail` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_1`(`cname`) USING BTREE,
+  INDEX `fk_2`(`phone`) USING BTREE,
+  CONSTRAINT `fk_1` FOREIGN KEY (`cname`) REFERENCES `tb_vip` (`cname`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_2` FOREIGN KEY (`phone`) REFERENCES `tb_vip` (`phone`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-create index fk_1
-  on tb_order (cname);
+-- ----------------------------
+-- Table structure for tb_room
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_room`;
+CREATE TABLE `tb_room`  (
+  `id` int(11) NOT NULL,
+  `type` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` double DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `start_time` datetime(0) DEFAULT NULL,
+  `end_time` datetime(0) DEFAULT NULL,
+  PRIMARY KEY (`type`, `id`) USING BTREE,
+  INDEX `index`(`type`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-create index fk_2
-  on tb_order (phone);
+-- ----------------------------
+-- Table structure for tb_vip
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_vip`;
+CREATE TABLE `tb_vip`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`, `cname`) USING BTREE,
+  INDEX `cname`(`cname`) USING BTREE,
+  INDEX `phone`(`phone`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-create index cname
-  on tb_vip (cname);
-
-create index phone
-  on tb_vip (phone);
-
+SET FOREIGN_KEY_CHECKS = 1;
